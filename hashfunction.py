6613e8f2014745385fb275643afff69428fff7c9
sonia-greco <144718680+sonia-greco@users.noreply.github.com>
2024-09-07 03:14:59 +0000

    set up basic outline

--- hashfunction.py
+++ hashfunction.py
@@ -1,1 +1,34 @@
+import hashlib
+
+#def createAccount(userName, password):
+   #print('Hello, hello')
+    #hashedpwd = hash(password)
+    #print(hashedpwd)
+    #add(userName, password)
+    
+#createAccount('Sonia', '123')
+
+#userName = input('Input username: ')
+#print('Username is ' + userName)
+#password = input('Input password: ')
+
+
+def createAccount(userName, password):
+	userName = input('Input username: ')
+	print('Username is ' + userName)
+	password = input('Input password: ')
+	hashedpwd = hash(password)
+	input('Input password' + hashedpwd)
+	createAccount.add(userName, hashedpwd)
+	print('Password is ' + hashedpwd)
+
+def verifyAccount(userName, password):
+	account = createAccount.get(userName)
+	if account == None:
+		return False
+	hashedpwd = hash(password)
+	if account.value == hashedpwd:
+		return True
+	return False
+
 
