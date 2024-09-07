2086f3e782fca28c6d8d45ec8073c665036c423f
sonia-greco <144718680+sonia-greco@users.noreply.github.com>
2024-09-07 22:45:48 +0000

    Hash function working with dictionary to save and check username and password

--- hashfunction.py
+++ hashfunction.py
@@ -1,34 +1,41 @@
-import hashlib
-
-#def createAccount(userName, password):
-   #print('Hello, hello')
-    #hashedpwd = hash(password)
-    #print(hashedpwd)
-    #add(userName, password)
-    
-#createAccount('Sonia', '123')
-
-#userName = input('Input username: ')
-#print('Username is ' + userName)
-#password = input('Input password: ')
-
+myMap = {}
 
 def createAccount(userName, password):
-	userName = input('Input username: ')
-	print('Username is ' + userName)
-	password = input('Input password: ')
 	hashedpwd = hash(password)
-	input('Input password' + hashedpwd)
-	createAccount.add(userName, hashedpwd)
-	print('Password is ' + hashedpwd)
+	dict2 = {userName: hashedpwd}
+	myMap.update(dict2)
+	print('Password is ' + str(hashedpwd))
 
 def verifyAccount(userName, password):
-	account = createAccount.get(userName)
-	if account == None:
+	toCheck = myMap.get(userName)
+	if toCheck == None:
+		print('Username not found')
 		return False
-	hashedpwd = hash(password)
-	if account.value == hashedpwd:
+	if toCheck == hash(password):
 		return True
+	print('Password not found')
 	return False
 
+print('Input username and password three times: ')
 
+for i in range(3):
+	userName = input('Input username: ')
+	password = input('Input password: ')
+	createAccount(userName, password)
+
+print(myMap)
+
+continueVerify = 'Y'
+
+while continueVerify == 'Y':
+
+	checkUsername = input('Please input username to verify account: ')
+	checkPassword = input('Please input password to verify account: ')
+
+	accountExists = verifyAccount(checkUsername, checkPassword)
+	if accountExists == True:
+		print('Account verified')
+	else:
+		print('Account not verified')
+
+	continueVerify = input('Do you want to continue? Y or N: ')
