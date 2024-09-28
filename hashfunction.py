6a3701a31fbc409732c20b7b2de5c6081e539541
sonia-greco <144718680+sonia-greco@users.noreply.github.com>
2024-09-28 03:01:09 +0000

    quick fixes and create new password complete

--- hashfunction.py
+++ hashfunction.py
@@ -1,5 +1,9 @@
 import sqlite3
-import os
+
+# TO DO
+# Remove hashmap in memory because it is not used anymore (we are using a database) 
+# Build a function to change password, which first verifies if the old password is correct and, if correct, it actually change it to the new one. We reuse the existing function "verifyAccount" to check if the old password is correct.
+# Use the new changePassword function in the main program to let the user actually update a password
 
 con = sqlite3.connect('hashedpotatoes.db')
 
@@ -18,12 +22,12 @@ CREATE TABLE IF NOT EXISTS myusertable (
 )
 ''')
 
-myMap = {}
+#myMap = {}
 
 def createAccount(userName, password):
 	hashedpwd = str(hash(password))
-	dict2 = {userName: hashedpwd}
-	myMap.update(dict2)
+#	dict2 = {userName: hashedpwd}
+#	myMap.update(dict2)
 	cur.execute("INSERT INTO myusertable (username, hash) VALUES (?, ?)", (userName, hashedpwd))
 	con.commit()
 	print('Password is ' + str(hashedpwd))
@@ -43,21 +47,33 @@ def verifyAccount(userName, password):
 		print('Password not found')
 		return False
 
-print('Input username and password: ')
+def changePassword(userName, password, newPassword):
+	accountExists = verifyAccount(userName, password)
+	if accountExists:
+		hashedpwd = str(hash(newPassword))
+		cur.execute("UPDATE myusertable SET hash = ? WHERE userName = ?", (hashedpwd, userName))
+		con.commit()
+		return True
+	else:
+		return False
+
+print('Please create an account')
 
 for i in range(1):
 	userName = input('Input username: ')
 	password = input('Input password: ')
 	createAccount(userName, password)
 
-print(myMap)
+#print(myMap)
 
-continueVerify = 'Y'
+continueVerify = 'V'
 
-while continueVerify == 'Y':
+while continueVerify == 'V':
 
-	checkUsername = input('Please input username to verify account: ')
-	checkPassword = input('Please input password to verify account: ')
+	print('Please verify account:')
+
+	checkUsername = input('Input USERNAME: ')
+	checkPassword = input('Input PASSWORD: ')
 
 	accountExists = verifyAccount(checkUsername, checkPassword)
 	if accountExists == True:
@@ -65,4 +81,23 @@ while continueVerify == 'Y':
 	else:
 		print('Account not verified')
 
-	continueVerify = input('Do you want to continue? Y or N: ')
+	continueVerify = input('Do you want to continue to verify or create a new password? \nV for verify or P password: ')
+
+createNewPwd = 'P'
+
+while createNewPwd == 'P':
+
+	currentuser = input('Input USERNAME: ')
+	currentpwd = input('Input CURRENT PASSWORD: ')
+
+	accountExists = verifyAccount(currentuser, currentpwd)
+	if accountExists == True:
+		newPassword = input('Please input new password: ')
+		passwordChanged = changePassword(currentuser, currentpwd, newPassword)
+	else:
+		print('Account not found')
+
+	createNewPwd = input('Do you want to change another password? \nP for yes and N for no: ')
+
+
+
